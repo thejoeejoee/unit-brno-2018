3cc2c6bc35313f52effda3e9fd6f77e44955719a
Josef Kolář <thejoeejoee@gmail.com>
2018-04-05 20:35:09 +0200

    Add deps and basic py structure

---
+++ main.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from argparse import ArgumentParser
+
+
+def create_arg_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+
+    return parser
+
+
+def main() -> int:
+    pass
+
+
+if __name__ == '__main__':
+    exit(main())
---
+++ unit/__init__.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+
+
+__author__ = 'Josef Kolář, Son Hai Nguyen, Tina Heindlová, Jan Vykydal'
+__licence__ = 'MIT'
+__version__ = version = '0.0.1'
