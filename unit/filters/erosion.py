a1cd2fa66441f1fd0b37acc8ae1ab1b20686b8a6
wykys <vvykys@gmail.com>
2018-04-06 14:09:30 +0200

    fidex conrer bug in erosion

--- unit/filters/erosion.py
+++ unit/filters/erosion.py
@@ -17,38 +17,40 @@ class Erosion(object):
                         move_x_minus = True
                         move_x_plus = True
                         for xx in range(self.minsize):
-                            if x - xx >= 0 and move_x_minus and img[x - xx][y]:
+                            if move_x_minus and x - xx >= 0 and img[x - xx][y]:
                                 cnt_x += 1
                             else:
                                 move_x_minus = False
 
-                            if x + xx < self.len_x and move_x_plus and img[x + xx][y]:
+                            if move_x_plus and x + xx < self.len_x and img[x + xx][y]:
                                 cnt_x += 1
                             else:
                                 move_x_plus = False
 
-                            if cnt_x >= self.minsize or not (move_x_plus or move_x_minus):
+                            if cnt_x-1 >= self.minsize or not (move_x_plus or move_x_minus):
                                 break
 
                         cnt_y = 0
                         move_y_minus = True
                         move_y_plus = True
                         for yy in range(self.minsize):
-                            if y - yy >= 0 and move_y_minus and img[x][y - yy]:
+                            if move_y_minus and y - yy >= 0 and img[x][y - yy]:
                                 cnt_y += 1
                             else:
                                 move_y_minus = False
 
-                            if y + yy < self.len_y and move_y_plus and img[x][y + yy]:
+                            if move_y_plus and y + yy < self.len_y and img[x][y + yy]:
                                 cnt_y += 1
                             else:
                                 move_y_plus = False
 
-                            if cnt_y >= self.minsize or not (move_y_plus or move_y_minus):
+                            if cnt_y-1 >= self.minsize or not (move_y_plus or move_y_minus):
                                 break
 
                         if cnt_x < self.minsize or cnt_y < self.minsize:
                             img[x][y] = False
+
+
 
         return img
 
@@ -73,19 +75,19 @@ if __name__ == '__main__':
         print('=' * len(img[0]))
 
     img = [
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
-        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
-        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
-        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
-        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
-        ['H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
-        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
-        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
-        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
-        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
-        #['.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'],
-        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.', '.'],
+        ['H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', 'H', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.', '.'],
     ]
 
     img = from_ascii_to_bool(img)
