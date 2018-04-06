ccb0f36674a120d0d18b3f7d2e6beede43a49603
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2018-04-06 15:26:25 +0200

    Add generic_filter, threshold, gaussian_filter and sobel

---
+++ unit/filters/edge_detection.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+import numpy as np
+from scipy.signal import convolve2d
+
+def generic_filter(source: np.ndarray, filter: np.ndarray) -> np.ndarray:
+    return convolve2d(source, filter)
+
+def gaussian_filter(source: np.ndarray) -> np.ndarray:
+    return generic_filter(source, np.matrix([
+        [2, 4, 5, 4, 2],
+        [4, 9, 12, 9, 4],
+        [5, 12, 15, 12, 5],
+        [4, 9, 12, 9, 4],
+        [2, 4, 5, 4, 2],
+    ])) * (1. / 159)
+
+def sobel(source: np.ndarray):
+    grad_x = generic_filter(source, np.matrix([
+        [1, 0, -1],
+        [2, 0, -2],
+        [1, 0 , -1]]
+    ))
+
+    grad_y = generic_filter(source, np.matrix([
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1]]
+    ))
+
+    grads = np.hypot(grad_x, grad_y)
+    thetas = np.arctan2(grad_y, grad_x)
+    return grads, thetas
+
+
--- unit/filters/threshold.py
+++ unit/filters/threshold.py
@@ -2,8 +2,6 @@
 
 import numpy as np
 
-
-
 def threshold_image(image: np.ndarray, threshold: int) -> np.ndarray:
     thresholder = lambda x: 255 if x > threshold else 0
     vfunc = np.vectorize(thresholder)
