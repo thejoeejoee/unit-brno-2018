362e4e638132c7b9a9e9016bef544ac368a123c4
wykys <vvykys@gmail.com>
2018-04-06 19:53:34 +0200

    total refactorized erosion filter

--- unit/processor.py
+++ unit/processor.py
@@ -4,12 +4,13 @@ from typing import Type, TextIO, Iterable
 import matplotlib.pyplot as plt
 import numpy
 
-from unit.detectors.gmm import GaussianMixtureModelDetector
+#from unit.detectors.gmm import GaussianMixtureModelDetector
 from unit.detectors.hough_circle import HoughCircleDetector
 from unit.exporters.base import BaseExporter
 from unit.exporters.csv import CsvExporter
 from unit.filters.edge_detection import gaussian_filter, sobel
 from unit.filters.threshold import threshold_image
+from unit.filters.erosion import erosion_filter
 from unit.loader import Loader
 from unit.particle import Particle
 
@@ -46,6 +47,7 @@ class Processor(object):
         plt.imshow(image, cmap='gray')
 
         image = threshold_image(image, 80)
+        image = erosion_filter(image, 15)
         image = gaussian_filter(image)
         grads, thetas = sobel(image)
         plt.subplot(222)
