289d912b8e3a4f8fc9bf29a6a45913616df0be75
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2018-04-06 22:02:28 +0200

    Add sony filter

--- unit/filters/edge_detection.py
+++ unit/filters/edge_detection.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import numpy as np
+from math import pi
 from scipy.signal import convolve2d
 
 
@@ -17,6 +18,25 @@ def gaussian_filter(source: np.ndarray) -> np.ndarray:
     ])) * (1. / 159)
 
 
+def sony(source: np.ndarray):
+    print(source)
+    averaged = generic_filter(source, np.matrix([
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 1, 1]]
+    ))
+
+    def normalize(x):
+        if x == 255 * 8:
+            return 0
+        elif x == 0:
+            return 0
+        else:
+            return 255
+
+    vfunc = np.vectorize(normalize)
+    return vfunc(averaged)
+
 def sobel(source: np.ndarray):
     grad_x = generic_filter(source, np.matrix([
         [1, 0, -1],
@@ -30,8 +50,23 @@ def sobel(source: np.ndarray):
         [-1, -2, -1]]
     ))
 
-    grads = np.hypot(grad_x, grad_y)
+    def normalize_angle(x: float) -> int:
+        x = round(x % 180)
+        if x >= 0 and x <= 22.5:
+            return 0
+        elif x > 22.5 and x <= 67.5:
+            return 45
+        elif x > 67.5 and x <= 112.5:
+            return 90
+        elif x > 112.5 and x <= 157.5:
+            return 135
+        elif x > 157.5 and x <= 180:
+            return 0
+
     thetas = np.arctan2(grad_y, grad_x)
+    #thetas = np.vectorize(normalize_angle)(thetas)
+
+    grads = np.hypot(grad_y, grad_x)
     return grads, thetas
 
 
