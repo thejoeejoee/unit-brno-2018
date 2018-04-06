7ca38c042e6ab4d7eb3dbed3c020c5c264fba6ff
wykys <vvykys@gmail.com>
2018-04-06 13:43:12 +0200

    updated erosion

--- unit/filters/erosion.py
+++ unit/filters/erosion.py
@@ -1,5 +1,4 @@
 import numpy as np
-import copy
 
 
 class Erosion(object):
@@ -10,47 +9,87 @@ class Erosion(object):
         self.minsize = minsize
 
     def filter(self):
-        out = copy.deepcopy(img)
-        for x in range(len_x):
-            for y in range(len_y):
-                if img[x][y]:
-                    cnt_x = 0
-                    move_x_minus = True
-                    move_x_plus = True
-                    for xx in range(self.minsize):
-                        if any((
-                            x - xx >= 0 and move_x_minus and img[x - xx][y],
-                            x + xx < self.len_x and move_x_plus and img[x + xx][y]
-                        )):
-                            cnt_x += 1
-                        else:
-                            move_x_minus = False
+        for i in range(2):
+            for x in range(self.len_x):
+                for y in range(self.len_y):
+                    if img[x][y]:
+                        cnt_x = 0
+                        move_x_minus = True
+                        move_x_plus = True
+                        for xx in range(self.minsize):
+                            if x - xx >= 0 and move_x_minus and img[x - xx][y]:
+                                cnt_x += 1
+                            else:
+                                move_x_minus = False
 
-                        if cnt_x >= self.minsize or not (move_x_plus or move_x_minus):
-                            break
+                            if x + xx < self.len_x and move_x_plus and img[x + xx][y]:
+                                cnt_x += 1
+                            else:
+                                move_x_plus = False
 
-                    cnt_y = 0
-                    move_y_minus = True
-                    move_y_plus = True
-                    for yy in range(self.minsize):
-                        if any((
-                            y - yy >= 0 and move_y_minus and img[x][y - yy],
-                            y + yy < self.len_y and move_y_plus and img[x][y + yy]
-                        )):
-                            cnt_y += 1
-                        else:
-                            move_x_minus = False
+                            if cnt_x >= self.minsize or not (move_x_plus or move_x_minus):
+                                break
 
-                        if cnt_y >= self.minsize or not (move_y_plus or move_y_minus):
-                            break
+                        cnt_y = 0
+                        move_y_minus = True
+                        move_y_plus = True
+                        for yy in range(self.minsize):
+                            if y - yy >= 0 and move_y_minus and img[x][y - yy]:
+                                cnt_y += 1
+                            else:
+                                move_y_minus = False
 
-                    if cnt_x < self.minsize and cnt_y < self.minsize:
-                        out[x][y] = False
+                            if y + yy < self.len_y and move_y_plus and img[x][y + yy]:
+                                cnt_y += 1
+                            else:
+                                move_y_plus = False
 
-        return out
+                            if cnt_y >= self.minsize or not (move_y_plus or move_y_minus):
+                                break
 
+                        if cnt_x < self.minsize or cnt_y < self.minsize:
+                            img[x][y] = False
 
+        return img
 
 
 if __name__ == '__main__':
-    pass
+
+    def from_ascii_to_bool(img):
+        out = img
+        for x in range(len(img)):
+            for y in range(len(img[0])):
+                out[x][y] = True if img[x][y] != '.' else False
+        return out
+
+    def ascii_plot(img):
+        for x in range(len(img)):
+            for y in range(len(img[0])):
+                if img[x][y]:
+                    print('H', end='')
+                else:
+                    print('.', end='')
+            print('')
+        print('=' * len(img[0]))
+
+    img = [
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+        ['H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+        #['.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'],
+        ['.', '.', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', '.', '.', '.', '.'],
+    ]
+
+    img = from_ascii_to_bool(img)
+    ascii_plot(img)
+    erosion = Erosion(img)
+    img = erosion.filter()
+    ascii_plot(img)
